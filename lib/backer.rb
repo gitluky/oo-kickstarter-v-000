52e56cab1b75c3197066a6c5437d598dcef870b7
Jonathan Luk <jluk87@gmail.com>
2018-03-23 17:07:09 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,16 @@
 require 'pry'
 class Backer
 
+  attr_accessor :name
+  attr_reader :backed_projects
 
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project if project.is_a?(Project)
+  end
+  
 end
