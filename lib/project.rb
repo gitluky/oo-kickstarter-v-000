860b470303057b498cc376d701992a20a479c570
Jonathan Luk <jluk87@gmail.com>
2018-03-23 17:12:46 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,15 @@
 class Project
   attr_accessor :title
+  attr_reader :backers
 
   def initialize(title)
     @title = title
     @backers = []
+  end
+
+  def add_backer(backer)
+    self.backers << backer if backer.is_a?(Backer)
+    backer.backed_projects << self unless backer.backed_projects.include?(self)
   end
 
 end
